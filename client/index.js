acce87259c866d22785d8a6a313f56416c51b9b5
Muslim Guseinov <gusejnov1999@list.ru>
2019-04-21 22:56:21 +0300

    workflow(client)[mod]: add canvas, render grid, cell

--- client/index.js
+++ client/index.js
@@ -1,5 +1,74 @@
-import * as wasm from "wasm-game-of-life";
+import { Universe, Cell } from "wasm-game-of-life";
+import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
 
-const res = wasm.plus(2, 2);
+const CELL_SIZE = 5;
+const GRID_COLOR = "#211f1f";
+const DEAD_COLOR = "#211f1f";
+const ALIVE_COLOR = "#3dc126";
+const UNIVERSAL_WIDTH = 124;
+const UNIVERSAL_HEIGHT = 124;
 
-wasm.greet(res);
+const universe = Universe.new(UNIVERSAL_WIDTH, UNIVERSAL_HEIGHT);
+const width = universe.width();
+const height = universe.height();
+
+const canvas = document.getElementById("game-of-life-canvas");
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+console.log(canvas.height);
+const ctx = canvas.getContext("2d");
+
+const drawGrid = () => {
+  ctx.beginPath();
+  ctx.strokeStyle = GRID_COLOR;
+
+  for (let i = 0; i <= width; i++) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  for (let j = 0; j <= height; j++) {
+    ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+  }
+
+  ctx.stroke();
+};
+
+const getIndex = (row, column) => {
+  return row * width + column;
+};
+
+const drawCells = () => {
+  const cellsPtr = universe.cells();
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+  ctx.beginPath();
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+
+      ctx.fillStyle = cells[idx] === Cell.Dead ? DEAD_COLOR : ALIVE_COLOR;
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  ctx.stroke();
+};
+
+const renderLoop = () => {
+  universe.tick();
+
+  drawGrid();
+  drawCells();
+  requestAnimationFrame(renderLoop);
+};
+
+renderLoop();
